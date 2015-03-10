4e0608e1d2c108b461b7a3dd098461b13ee770e9
Derek Graham <deejaygraham@gmail.com>
2015-03-10 12:38:32 +0000

    initial version

---
+++ UnpackCsPkg/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace UnpackCsPkg
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("UnpackCsPkg <file>");
+                return 1;
+            }
+
+            string package = args[0];
+
+            if (!File.Exists(package))
+            {
+                Console.WriteLine("Package file : {0} does not exist", package);
+                return 2;
+            }
+
+            try
+            {
+                Console.WriteLine("Unpacking {0}", package);
+
+                string extractFolder = UnzipFileToFolder(package);
+
+                foreach (string extractFile in Directory.GetFiles(extractFolder, "*.*x"))
+                {
+                    UnzipFileToFolder(extractFile);
+                }
+
+                Console.WriteLine("Done");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return 0;
+        }
+
+        private static string UnzipFileToFolder(string path)
+        {
+            string cleanPath = path.RemoveGuid();
+
+            string containingFolder = Path.GetDirectoryName(path);
+            string extractFolder = Path.Combine(containingFolder, Path.GetFileNameWithoutExtension(cleanPath));
+
+            if (Directory.Exists(extractFolder))
+                Directory.Delete(extractFolder, recursive: true);
+
+            System.IO.Compression.ZipFile.ExtractToDirectory(path, extractFolder);
+
+            RenameGuidFilesInFolder(extractFolder);
+
+            return extractFolder;
+        }
+
+        private static void RenameGuidFilesInFolder(string folder)
+        {
+            foreach (string manifest in Directory.GetFiles(folder, "*.csman"))
+            {
+                string withoutGuid = manifest.RemoveGuid();
+
+                if (!File.Exists(withoutGuid))
+                {
+                    File.Copy(manifest, withoutGuid);
+                    File.Delete(manifest);
+                }
+            }
+
+            foreach (string zipContent in Directory.GetFiles(folder, "*.cs*x"))
+            {
+                string withoutGuid = zipContent.RemoveGuid();
+
+                if (!File.Exists(withoutGuid))
+                {
+                    File.Copy(zipContent, withoutGuid);
+                    File.Delete(zipContent);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ UnpackCsPkg/StringExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace UnpackCsPkg
+{
+    public static class StringExtensions
+    {
+        public static string RemoveGuid(this string path)
+        {
+            string containingFolder = Path.GetDirectoryName(path);
+            string filenameOnly = Path.GetFileNameWithoutExtension(path);
+
+            // assuming no other underscores...
+            const char Underscore = '_';
+
+            int firstUnderscore = filenameOnly.IndexOf(Underscore);
+
+            // is the name in the format <name>_guid ?
+            if (firstUnderscore > 0)
+            {
+                string candidateGuid = filenameOnly.Substring(firstUnderscore + 1);
+
+                if (candidateGuid.IsInGuidFormat())
+                {
+                    filenameOnly = filenameOnly.Substring(0, firstUnderscore);
+                }
+            }
+            else
+            {
+                // maybe it's all guid ?
+                string filename = Path.GetFileNameWithoutExtension(path);
+
+                if (filename.IsInGuidFormat())
+                {
+                    // take the name from the containing folder.
+                    filenameOnly = new DirectoryInfo(containingFolder).Name;
+                }
+            }
+
+            return Path.Combine(containingFolder, filenameOnly + Path.GetExtension(path));
+        }
+
+        public static bool IsInGuidFormat(this string candidate)
+        {
+            try
+            {
+                new Guid(candidate);
+                return true;
+            }
+            catch
+            {
+                // not really a guid..
+            }
+
+            return false;
+        }
+    }
+}
